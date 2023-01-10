9f4c36cb70c1ebf6276f271596284f8d4627830e
godsakani <nyonggodwill11@gmail.com>
2023-01-10 18:52:38 +0100

    folder: added lib folder

---
+++ lib/Styles/colors.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFFE6682E);
+const Color primaryColor1 = Color(0xFFEB895B);
+const Color primaryColor2 = Color(0xFFF1A989);
+const Color primaryColor3 = Color(0xFFF6CAB6);
+const Color secondaryColor = Color(0xFF00A98C);
+const Color lightColor = Color(0xFF00DBB6);
+const Color neutralColor = Color(0xFF212121);
+const Color errorColor = Color(0xFFEE0202);
+const Color successColor = Color(0xFF00AA44);
+const Color backgroundColor = Color(0xFFDDDDDD);
+
+
